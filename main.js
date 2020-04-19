2184a7d36b548b62e2a6141008afc8c5789ccaa3
beatty6 <beatty6@pdx.edu>
2020-04-18 17:41:59 -0700

    added jQuery code

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
  
     
     $("button#button-one").click(function(){
-    var one = $("div#one");  
+    var one = $("div#one"); 
+        var sp = $("#sp");
+        var zoom = $("#zoom");
        one.css({visibility: 'visible'}, 5000);
        one.animate({opacity: '1'}, "slow");
     one.animate({left: '100px'}, "slow");
     one.animate({fontSize: '3em'}, "slow");
+        
+       
+        sp.animate({opacity:'1'},5000);
+          zoom.animate({opacity:'1'},5000);
   });
+
     
     $("button#button-two").click(function(){
          var one = $("div#one");  

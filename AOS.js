cb1325ecde0b30fe7da034e057adf84f92b0ade8
beatty6 <beatty6@pdx.edu>
2020-04-30 08:32:24 -0700

    cool

---
+++ AOS.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    var fadeStart = 100 // 100px scroll or less will equiv to 1 opacity
+        ,
+        fadeUntil = 200 // 200px scroll or more will equiv to 0 opacity
+        ,
+        fading = $('.graphic');
+
+    $(window).bind('scroll', function () {
+        var offset = $(document).scrollTop(),
+            opacity = 0;
+        if (offset <= fadeStart) {
+            opacity = 1;
+        } else if (offset <= fadeUntil) {
+            opacity = 1 - offset / fadeUntil;
+        }
+        fading.css('opacity', opacity).html(opacity);
+    });
+});
--- main.js
+++ main.js
@@ -1,35 +1,31 @@
-$(document).ready(function(){
- 
-    
-    $("button#button-one").click(function(){
-    var one = $("div#one"); 
+$(document).ready(function () {
+
+
+    $("button#button-one").click(function () {
+        var one = $("div#one");
         var sp = $("#sp");
         var zoom = $("#zoom");
-       one.css({visibility: 'visible'}, 5000);
-       one.animate({opacity: '1'}, "slow");
-    one.animate({left: '100px'}, "slow");
-    one.animate({fontSize: '3em'}, "slow");
-        
-       
-        sp.animate({opacity:'1'},5000);
-          zoom.animate({opacity:'1'},5000);
-  });
-
-    
-    $("button#button-two").click(function(){
-         var one = $("div#one");  
-    var two = $("div#two");  
-         one.css({visibility: 'hidden'});
-         one.css({position: 'absolute'});
-       two.css({visibility: 'visible'}, 5000);
-       two.animate({opacity: '1'}, "slow");
-    two.animate({left: '100px'}, "slow");
-    two.animate({fontSize: '3em'}, "slow");
-  });
+        one.css({
+            visibility: 'visible'
+        }, 5000);
+        one.animate({
+            opacity: '1'
+        }, "slow");
+        one.animate({
+            left: '100px'
+        }, "slow");
+        one.animate({
+            fontSize: '3em'
+        }, "slow");
 
 
-
-
+        sp.animate({
+            opacity: '1'
+        }, 5000);
+        zoom.animate({
+            opacity: '1'
+        }, 5000);
+    });
 
 
 });
